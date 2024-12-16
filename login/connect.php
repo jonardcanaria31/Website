fe5550c556044a8ac41f5c5db0245bd18dedd6fe
jonardcanaria31 <111260081+jonardcanaria31@users.noreply.github.com>
2024-12-16 02:43:07 -1200

    pogi

--- login/connect.php
+++ login/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 $host="localhost";
-$user="root";
-$pass="";
-$db="login";
+$user="u143688490_jonard";
+$pass="January31,2004";
+$db="u143688490_Goffee	";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
